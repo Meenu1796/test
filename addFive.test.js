d300c064d72b4000eacef1a9c3b59722f17efa40
Meenu <meenucj1796@email.com>
2025-12-26 20:03:48 +0000

    Added new test condition

--- addFive.test.js
+++ addFive.test.js
@@ -1,5 +1,7 @@
 const addFive = require(`./addFive`);
 
 test('returns the number plus 5', () => {
-    expect(addFive(1)).toBe(6);
+    //expect(addFive(1)).toBe(6);
+    expect(addFive(3)).toBe(8);
+    //expect(addFive(4)).toBe(9);
 })
